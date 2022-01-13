c12c7dc1bfe8cb53aabcf6bbc3a4fa367b379ed5
wangyangtao <wangyangtao@xdf.cn>
2022-01-13 14:00:34 +0800

    feat: first commit

---
+++ examples/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render} from 'react-dom';
+import MyComponent from '../../src';
+const App = () => (
+ <MyComponent size='36' color='red' />
+);
+render(<App />, document.getElementById("root"));
